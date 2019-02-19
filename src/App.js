ef0f006c2a962fc30769ebef3d32e9413bf6da5e
fedea <fedeaperez@hotmail.com.ar>
2019-02-18 23:53:29 -0300

    only leagus name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { SoccerApi } from './models/soccer-api';
 import './match.css';
-
+const getCompetitionName = str => str.split(':')[1].trim();
 class App extends Component {
   constructor(props) {
     super(props);
@@ -14,11 +14,11 @@ class App extends Component {
     let socApi = new SoccerApi();
     let socApiVids = await socApi.getVideos();
     console.table(socApiVids);
-    let Partido = socApiVids[0];
-    console.table(Partido);
-    console.table(Partido.side1)
-    console.table(Partido.side2);
-    console.table(Partido.competition)
+    //let Partido = socApiVids[0];
+    //console.table(Partido);
+    //console.table(Partido.side1)
+    //console.table(Partido.side2);
+    //console.table(Partido.competition)
     this.setState((state, props) => {
       return {
         matchs: socApiVids
@@ -33,10 +33,10 @@ class App extends Component {
           this.state.matchs && this.state.matchs.length > 0 &&
           this.state.matchs.map((elem, index) => {
             return (
-            <section key={"match_"+index} className="match">
+            <section className="match" key={"match_"+index}>
               <span key={'match_side1_'+index}>{elem.side1.name}</span>
               <span key={'match_side2_'+index}>{elem.side2.name}</span>
-              <span key={'match_competition_'+index}>{elem.competition.name}</span>
+              <span className="match_competition" key={'match_competition_'+index}>{getCompetitionName(elem.competition.name)}</span>
             </section>
             );
           })
