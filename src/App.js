d58c6480684fb00b51281d34ca165017c6b2d8c8
fedea <fedeaperez@hotmail.com.ar>
2019-02-18 23:28:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SoccerApi } from './models/soccer-api';
+import './match.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  async componentDidMount() {
+    let socApi = new SoccerApi();
+    let socApiVids = await socApi.getVideos();
+    console.table(socApiVids);
+    let Partido = socApiVids[0];
+    console.table(Partido);
+    console.table(Partido.side1)
+    console.table(Partido.side2);
+    console.table(Partido.competition)
+    this.setState((state, props) => {
+      return {
+        matchs: socApiVids
+      };
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="matchs">
+        {
+          this.state.matchs && this.state.matchs.length > 0 &&
+          this.state.matchs.map((elem, index) => {
+            return (
+            <section key={"match_"+index} className="match">
+              <span key={'match_side1_'+index}>{elem.side1.name}</span>
+              <span key={'match_side2_'+index}>{elem.side2.name}</span>
+              <span key={'match_competition_'+index}>{elem.competition.name}</span>
+            </section>
+            );
+          })
+        }
       </div>
     );
   }
