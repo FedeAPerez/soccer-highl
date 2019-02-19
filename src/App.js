a1d32d86dfbd14f32a43e5b95b61e9b2721dc956
fedea <fedeaperez@hotmail.com.ar>
2019-02-19 00:01:47 -0300

    basic query of matches

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { SoccerApi } from './models/soccer-api';
 import './match.css';
+
 const getCompetitionName = str => str.split(':')[1].trim();
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -33,10 +35,11 @@ class App extends Component {
           this.state.matchs && this.state.matchs.length > 0 &&
           this.state.matchs.map((elem, index) => {
             return (
-            <section className="match" key={"match_"+index}>
-              <span key={'match_side1_'+index}>{elem.side1.name}</span>
-              <span key={'match_side2_'+index}>{elem.side2.name}</span>
-              <span className="match_competition" key={'match_competition_'+index}>{getCompetitionName(elem.competition.name)}</span>
+            <section className="match" key={"match-"+index}>
+              <h4 className="match-sides" key={'match-sides-'+index}>{elem.side1.name} - {elem.side2.name}</h4>
+              <h3 className="match-competition" key={'match-competition-'+index}>
+                {getCompetitionName(elem.competition.name)}
+              </h3>
             </section>
             );
           })
