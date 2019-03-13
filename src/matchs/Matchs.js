d9d8ecf98ef067c88e88774afbccfe324f068e7b
fedea <fedeaperez@hotmail.com.ar>
2019-03-13 17:27:21 -0300

    matchs

--- src/matchs/Matchs.js
+++ src/matchs/Matchs.js
@@ -2,29 +2,44 @@ import React from 'react';
 import { useSoccerVideos } from '../hooks/soccer-hooks';
 import './match.css';
 
-const getCompetitionName = competition => competition.name.split(':')[1].trim();
+const getCompetitionName = competition => {
+  let countryCompetitionArray = competition.name.split(':');
+  let country = countryCompetitionArray[0].split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  return country + ' - ' + countryCompetitionArray[1].trim();
+}
 const getSidesName = match => `${match.side1.name} - ${match.side2.name}`;
 
 
 const Matchs = () => {
   const matchs = useSoccerVideos();
   console.table(matchs);
-  
+
   return (
     <div className="matchs">
       {
         matchs.length > 0 &&
         matchs.map((elem, index) => {
           return (
-          <section className="match" key={"match-"+index}>
-            <h3 className="match-sides" key={'match-sides-'+index}>
-              {getSidesName(elem)}
-            </h3>
+            <section className="match" key={"match-"+index}>
+              <img 
+                alt={`match-tumbnail`} 
+                src={elem.thumbnail} 
+                style={{
+                  width: '200px', 
+                  height: '150px', 
+                  display: 'block',
+                  margin: '1rem auto'
+                }}
+              />
+              <h3 className="match-sides" key={'match-sides-'+index}>
+                {getSidesName(elem)}
+              </h3>
 
-            <h4 className="match-competition" key={'match-competition-'+index}>
-              {getCompetitionName(elem.competition)}
-            </h4>
-          </section>
+              <h4 className="match-competition" 
+                key={'match-competition-'+index}>
+                { getCompetitionName(elem.competition) }
+              </h4>
+            </section>
           );
         })
       }
