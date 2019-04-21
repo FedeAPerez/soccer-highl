c91e865b7130ad4638435085e75f11bc10098ed7
Federico Pérez <federico.fperez@mercadolibre.com>
2019-04-21 20:55:12 -0300

    red

--- src/components/tag/tag.js
+++ src/components/tag/tag.js
@@ -1,20 +1,22 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import classNames from "classnames";
 import "./tag.scss";
 
 const namespace = "fut-tag";
 
 const Tag = ({ children, modifier }) => (
-  <span className={classNames(namespace, `${namespace}--${modifier}`)}>{children}</span>
+  <span className={classNames(namespace, `${namespace}--${modifier}`)}>
+    {children}
+  </span>
 );
 
 Tag.propTypes = {
-    modifier : PropTypes.oneOf(['green', 'red'])
+  modifier: PropTypes.oneOf(["green", "red"])
 };
 
 Tag.defaultProps = {
-    modifier: 'red';
-}
+  modifier: "red"
+};
 
 export default Tag;
